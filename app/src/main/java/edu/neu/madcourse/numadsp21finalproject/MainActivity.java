70f924e2d5af52611cc1643c7aaedaa328c01a73
Aishwarya Chauhan <chauhan.ai@husky.neu.edu>
2021-03-29 20:39:50 -0400

    added home activity

--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/MainActivity.java
@@ -2,13 +2,23 @@ package edu.neu.madcourse.numadsp21finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.login_btn);
+        loginButton.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), HomeActivity.class);
+            startActivity(intent);
+        });
     }
 }
