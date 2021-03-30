f038180cbfa5b2f00d3fcbccaf5c5d690379a3b0
libing <li.bi@northeastern.edu>
2021-03-30 02:13:48 -0400

    "set up internet check and log in listener

--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/MainActivity.java
@@ -3,12 +3,64 @@ package edu.neu.madcourse.numadsp21finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.neu.madcourse.numadsp21finalproject.utils.Helper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userNameInput;
+    private EditText userPasswordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.main_button_logIn);
+        if (Helper.getLoggedIn(this)) {
+            logIn(login, Helper.getUserName(this), Helper.getPassword(this));
+        }else {
+            Toast.makeText(this, "Please register or sign in", Toast.LENGTH_SHORT).show();
+        }
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!Helper.isOnline(MainActivity.this)) {
+                    Toast.makeText(MainActivity.this, Helper.NO_INTERNET,
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                userNameInput = findViewById(R.id.main_input_username);
+                userPasswordInput = findViewById(R.id.main_input_password);
+                String userName = userNameInput.getText().toString();
+                String password = userPasswordInput.getText().toString();
+                if (userName.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter your user name.",
+                            Toast.LENGTH_SHORT).show();
+                } if (password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter your password.",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    Helper.setUserNamePassword(MainActivity.this, userName, password);
+                    logIn(view, userName, password);
+
+                }
+            }
+        });
+        if(!Helper.isOnline(this)) {
+            Toast.makeText(this, Helper.NO_INTERNET,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void logIn(View view, String userName, String password) {
+
     }
 }
---
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/utils/Helper.java
@@ -0,0 +1,73 @@
+package edu.neu.madcourse.numadsp21finalproject.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
+
+public class Helper {
+    public static final String NO_INTERNET = "No internet connection";
+    public static String SERVER_KEY = "AAAAf-Efg5c:APA91bH5jUEnPT04fw-qdgAMl5ghx_ZAgNL6x4cMKoxz9MRoMEXYS2g4UHOEEuDs2Eb2ysEFfgtp48D8oxMpCiUt7ir6ezA09tv0FjvyH1mQ6jUtLkO_4_xadmzKlgQXfmNVmXgGoarB";
+
+    private final static String USER_NAME = "userName";
+    private final static String PASSWORD = "password";
+    private final static String LOGGED_IN = "loggedIn";
+    private final static String USER_TOKEN = "userToken";
+
+    //citation: https://stackoverflow.com/a/51070246
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm =  (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    //citation : https://stackoverflow.com/a/12744408
+    public static SharedPreferences getSharedPreferences(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx.getApplicationContext());
+    }
+
+    public static void setUserNamePassword(Context ctx, String userName, String password)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx.getApplicationContext()).edit();
+        editor.putString(USER_NAME, userName);
+        editor.putString(PASSWORD, password);
+        editor.putBoolean(LOGGED_IN, true);
+        editor.apply();
+    }
+
+
+    public static void setUserToken(Context ctx, String userToken) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx.getApplicationContext()).edit();
+        editor.putString(USER_TOKEN, userToken);
+        editor.apply();
+    }
+
+    public static String getUserName(Context ctx)
+    {
+        return getSharedPreferences(ctx.getApplicationContext()).getString(USER_NAME, "");
+    }
+
+    public static String getPassword(Context ctx)
+    {
+        return getSharedPreferences(ctx.getApplicationContext()).getString(PASSWORD, "");
+    }
+
+    public static Boolean getLoggedIn(Context ctx)
+    {
+        return getSharedPreferences(ctx.getApplicationContext()).getBoolean(LOGGED_IN, false);
+    }
+
+    public static void clearLoggedInName(Context ctx)
+    {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx.getApplicationContext()).edit();
+        editor.clear(); //clear all stored data
+        editor.apply();
+    }
+
+    public static String getUserToken(Context ctx) {
+        return getSharedPreferences(ctx.getApplicationContext()).getString(USER_TOKEN, "");
+    }
+
+
+}
