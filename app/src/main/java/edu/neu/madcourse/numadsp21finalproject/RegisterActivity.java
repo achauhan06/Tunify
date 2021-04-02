3c75f0c8f98b9a6e5d57e8e37ded16cd3b4c5c6b
santosh <tirthahalli.s@northeastern.edu>
2021-04-02 01:30:01 -0400

    added genre select UI

--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/RegisterActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/RegisterActivity.java
@@ -1,16 +1,23 @@
 package edu.neu.madcourse.numadsp21finalproject;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.text.format.Time;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -20,7 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
-        edittext = (EditText) findViewById(R.id.dob);
+        edittext = (EditText) findViewById(R.id.register_dob);
 
         edittext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +44,71 @@ public class RegisterActivity extends AppCompatActivity {
                 Time date = new Time();
                 DatePickerDialog d = new DatePickerDialog(RegisterActivity.this, dpd, date.year ,date.month, date.monthDay);
                 d.show();
+            }
+        });
+
+        EditText bOpenAlertDialog = findViewById(R.id.openAlertDialogButton);
+        final TextView tvSelectedItemsPreview = findViewById(R.id.selectedItemPreview);
+
+        final String[] listItems = new String[]{"Rock", "Pop", "Hip Hop", "Blues", "Jazz", "Reggae", "Folk", "Country", "Classical",
+        "Soul", "R&B", "Heavy Metal"};
+        final boolean[] checkedItems = new boolean[listItems.length];
+
+        final List<String> selectedItems = Arrays.asList(listItems);
+
+        bOpenAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tvSelectedItemsPreview.setText(null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+
+                builder.setTitle("Choose Genres:");
+
+
+                builder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        checkedItems[which] = isChecked;
+                        String currentItem = selectedItems.get(which);
+                    }
+                });
+
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            if (checkedItems[i]) {
+                                tvSelectedItemsPreview.setText(tvSelectedItemsPreview.getText() + selectedItems.get(i) + ", ");
+                            }
+                        }
+                    }
+                });
+
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.setNeutralButton("CLEAR ALL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for (int i = 0; i < checkedItems.length; i++) {
+                            checkedItems[i] = false;
+                        }
+                    }
+                });
+
+                builder.create();
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
             }
         });
     }
