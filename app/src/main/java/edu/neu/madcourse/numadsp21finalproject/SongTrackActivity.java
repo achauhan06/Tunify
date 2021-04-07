241171d4db3eee170ac4a0609c3fb861ac722065
Aishwarya Chauhan <chauhan.ai@husky.neu.edu>
2021-04-07 02:31:19 -0400

    record button

--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/SongTrackActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/SongTrackActivity.java
@@ -1,17 +1,30 @@
 package edu.neu.madcourse.numadsp21finalproject;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toolbar;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.io.IOException;
 
 import edu.neu.madcourse.numadsp21finalproject.utils.Helper;
 
@@ -21,6 +34,18 @@ public class SongTrackActivity extends YouTubeBaseActivity {
     private String songUrl;
     private String songName;
     private ImageButton youtubeBackButton;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private ImageButton recordButton = null;
+    private MediaRecorder recorder = null;
+
+   // private PlayButton   playButton = null;
+    private MediaPlayer player = null;
+
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private static String fileName = null;
+    private static final String LOG_TAG = "AudioRecordTest";
+    boolean mStartRecording = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +63,28 @@ public class SongTrackActivity extends YouTubeBaseActivity {
         createYoutubeView();
         youtubeBackButton = findViewById(R.id.youtubeBackButton);
         youtubeBackButton.setOnClickListener(v-> this.finish());
+        fileName = getExternalCacheDir().getAbsolutePath();
+        fileName += "/audiorecordtest.3gp";
+        setRecordSection();
+    }
+
+    private void setRecordSection() {
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        recordButton = findViewById(R.id.record);
+        recordButton.setOnClickListener(new View.OnClickListener(
+
+        ) {
+            @Override
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    recordButton.setImageResource(R.drawable.microphone);
+                } else {
+                    recordButton.setImageResource(R.drawable.microphone_block);
+                }
+                mStartRecording = !mStartRecording;
+            }
+        });
     }
 
     private void createYoutubeView() {
@@ -70,5 +117,95 @@ public class SongTrackActivity extends YouTubeBaseActivity {
         onBackPressed();
         return true;
     }*/
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(fileName);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+
+/*    class PlayButton extends androidx.appcompat.widget.AppCompatButton {
+        boolean mStartPlaying = true;
+
+        View.OnClickListener clicker = new View.OnClickListener() {
+            public void onClick(View v) {
+                onPlay(mStartPlaying);
+                if (mStartPlaying) {
+                    setText("Stop playing");
+                } else {
+                    setText("Start playing");
+                }
+                mStartPlaying = !mStartPlaying;
+            }
+        };
+
+        public PlayButton(Context ctx) {
+            super(ctx);
+            setText("Start playing");
+            setOnClickListener(clicker);
+        }
+    }*/
+
 
 }
