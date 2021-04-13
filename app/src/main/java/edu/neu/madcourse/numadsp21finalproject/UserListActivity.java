203b2fe77d0a75ef32d8f509e83f946b6903a0ff
santosh <tirthahalli.s@northeastern.edu>
2021-04-13 01:34:34 -0400

    User list

--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/UserListActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/UserListActivity.java
@@ -2,13 +2,20 @@ package edu.neu.madcourse.numadsp21finalproject;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import edu.neu.madcourse.numadsp21finalproject.songview.SongAdapter;
 import edu.neu.madcourse.numadsp21finalproject.songview.SongItem;
@@ -23,19 +30,38 @@ public class UserListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private UserAdapter userAdapter;
     private ArrayList<UserItem> userItemList;
+    private FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.users);
         userItemList = new ArrayList<>();
+        db = FirebaseFirestore.getInstance();
+
 
         Toolbar toolbar = findViewById(R.id.toolbar_user);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         // userItemList = getIntent().getParcelableArrayListExtra("users");
+        db.collection("users").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot d : list) {
+                                UserItem users = d.toObject(UserItem.class);
+                                userItemList.add(users);
+                            }
+                        } else {
+                            Toast.makeText(UserListActivity.this, "No data found in Database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
         createRecyclerView();
     }
 
