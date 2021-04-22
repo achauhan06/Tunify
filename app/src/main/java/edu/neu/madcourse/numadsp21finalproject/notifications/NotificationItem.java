2f84bdfc3c97ff53e2682ab02794c6d73a42f0df
santosh <tirthahalli.s@northeastern.edu>
2021-04-21 20:38:26 -0400

    Notification Recycle view

---
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/notifications/NotificationItem.java
@@ -0,0 +1,23 @@
+package edu.neu.madcourse.numadsp21finalproject.notifications;
+
+public class NotificationItem {
+
+    private String notificationItem;
+    private String notificationToken;
+
+    public String getNotificationItem() {
+        return notificationItem;
+    }
+
+    public void setNotificationItem(String notificationItem) {
+        this.notificationItem = notificationItem;
+    }
+
+    public String getNotificationToken() {
+        return notificationToken;
+    }
+
+    public void setNotificationToken(String notificationToken) {
+        this.notificationToken = notificationToken;
+    }
+}
