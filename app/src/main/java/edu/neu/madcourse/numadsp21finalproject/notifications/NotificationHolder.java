b246fcfc13c19d76e740c534974c6b0d8342e9f8
santosh <tirthahalli.s@northeastern.edu>
2021-04-21 21:50:13 -0400

    adapter and holder notifications

--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/notifications/NotificationHolder.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/notifications/NotificationHolder.java
@@ -3,6 +3,7 @@ package edu.neu.madcourse.numadsp21finalproject.notifications;
 import android.content.Context;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,13 +12,15 @@ import edu.neu.madcourse.numadsp21finalproject.R;
 import edu.neu.madcourse.numadsp21finalproject.songview.SongViewListener;
 
 public class NotificationHolder extends RecyclerView.ViewHolder {
-    private Button accept;
-    private Button decline;
+    public TextView notificationText;
+    public Button accept;
+    public Button decline;
     public NotificationHolder(@NonNull View itemView, Context context,
                               final NotificationViewListener notificationViewListener) {
         super(itemView);
         accept = itemView.findViewById(R.id.accept_button);
         decline = itemView.findViewById(R.id.decline_button);
+        notificationText = itemView.findViewById(R.id.notificationName);
         accept.setOnClickListener(v -> {
             if (notificationViewListener != null) {
                 int position = getLayoutPosition();
