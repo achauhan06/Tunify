3816fd0cc768066e77815dfe6b07157453844361
libing <li.bi@northeastern.edu>
2021-04-22 00:12:09 -0400

    fix merge conflict

--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/notifications/NotificationItem.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/notifications/NotificationItem.java
@@ -1,20 +1,16 @@
 package edu.neu.madcourse.numadsp21finalproject.notifications;
 
 import android.content.Context;
-<<<<<<< Updated upstream
 
 import java.sql.Time;
 import java.sql.Timestamp;
 
-public class NotificationItem implements NotificationViewListener{
-=======
 import android.content.Intent;
 import android.os.Parcelable;
 
 import edu.neu.madcourse.numadsp21finalproject.UserProfileActivity;
 
 public class NotificationItem implements NotificationViewListener {
->>>>>>> Stashed changes
 
     private String notificationItem;
     private String notificationToken;
@@ -40,21 +36,11 @@ public class NotificationItem implements NotificationViewListener {
         this.notificationToken = notificationToken;
     }
 
-<<<<<<< Updated upstream
     public Timestamp getTimestamp() {
         return timestamp;
     }
 
     @Override
     public void onItemClick(int position, Context context) {
-
-=======
-    @Override
-    public void onItemClick(int position, Context context) {
-        Intent intent = new Intent(context, NotificationListActivity.class);
-        //intent.putExtra("email", email);
-        //intent.putExtra("profileLink", profileLink);
-        context.startActivity(intent);
->>>>>>> Stashed changes
     }
 }
--- app/src/main/java/edu/neu/madcourse/numadsp21finalproject/notifications/NotificationListActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsp21finalproject/notifications/NotificationListActivity.java
@@ -1,13 +1,10 @@
 package edu.neu.madcourse.numadsp21finalproject.notifications;
 
-<<<<<<< Updated upstream
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-=======
 import android.content.Context;
->>>>>>> Stashed changes
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -16,16 +13,14 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-<<<<<<< Updated upstream
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+
 
 import edu.neu.madcourse.numadsp21finalproject.R;
-import edu.neu.madcourse.numadsp21finalproject.categoryview.CategoryViewListener;
-import edu.neu.madcourse.numadsp21finalproject.feedsview.FeedsAdapter;
-import edu.neu.madcourse.numadsp21finalproject.feedsview.FeedsItem;
-import edu.neu.madcourse.numadsp21finalproject.feedsview.FeedsViewListener;
+
 import edu.neu.madcourse.numadsp21finalproject.utils.MyBroadcastReceiver;
 
 public class NotificationListActivity extends AppCompatActivity {
@@ -36,35 +31,12 @@ public class NotificationListActivity extends AppCompatActivity {
 
     private BroadcastReceiver myBroadcastReceiver = null;
 
-
-=======
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import edu.neu.madcourse.numadsp21finalproject.R;
-import edu.neu.madcourse.numadsp21finalproject.UserListActivity;
-import edu.neu.madcourse.numadsp21finalproject.users.UserAdapter;
-import edu.neu.madcourse.numadsp21finalproject.users.UserItem;
-import edu.neu.madcourse.numadsp21finalproject.users.UserViewListener;
-
-public class NotificationListActivity extends AppCompatActivity {
-
-    private RecyclerView.LayoutManager rLayoutManger;
-    private RecyclerView recyclerView;
-    private NotificationAdapter notificationAdapter;
     private ArrayList<NotificationItem> notificationItemList;
-    private FirebaseFirestore db;
->>>>>>> Stashed changes
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications);
-<<<<<<< Updated upstream
         Toolbar toolbar = findViewById(R.id.toolbar_notifications);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -74,36 +46,14 @@ public class NotificationListActivity extends AppCompatActivity {
         broadcastIntent();
 
         notificationItemArrayList = new ArrayList<>();
-        NotificationItem item = new NotificationItem();
+        NotificationItem item = new NotificationItem("test");
+
         notificationItemArrayList.add(item);
         notificationItemArrayList.add(item);
         createNotificationRecyclerView();
-=======
-        Toolbar toolbar = findViewById(R.id.toolbar_song);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         notificationItemList = new ArrayList<>();
-        db = FirebaseFirestore.getInstance();
-        db.collection("notifications").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                if (!queryDocumentSnapshots.isEmpty()) {
-                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
-                    for (DocumentSnapshot d : list) {
-                        String notificationItem = d.getString("type");
-                        NotificationItem notification = new NotificationItem(notificationItem);
-                        notificationItemList.add(notification);
-                    }
-                } else {
-                    Toast.makeText(NotificationListActivity.this, "No data found in Database", Toast.LENGTH_SHORT).show();
-                }
-                createRecyclerView();
-            }
-        });
     }
-
     private void createRecyclerView() {
         rLayoutManger = new LinearLayoutManager(this);
         recyclerView = findViewById(R.id.notifications_recycler_view);
@@ -129,7 +79,6 @@ public class NotificationListActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
->>>>>>> Stashed changes
     }
 
     private void createNotificationRecyclerView() {
@@ -160,17 +109,7 @@ public class NotificationListActivity extends AppCompatActivity {
         recyclerView.setAdapter(notificationAdapter);
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        this.finish();
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
-    }
     public void broadcastIntent() {
         registerReceiver(myBroadcastReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
     }
